b68f5fa37b6f8d8b3200a95f0381bc161bf21442
Radyusha <Al.shel@bk.ru>
2023-10-12 11:09:24 +0300

    add java docs

--- module01/src/main/java/ru/sberbank/edu/impl/GCDImpl.java
+++ module01/src/main/java/ru/sberbank/edu/impl/GCDImpl.java
@@ -2,7 +2,18 @@ package ru.sberbank.edu.impl;
 
 import ru.sberbank.edu.CommonDivisor;
 
+/**
+ * Класс GCDImpl реализует интерфейс CommonDivisor
+ * @author Максим Радюшин{
+ */
 public class GCDImpl implements CommonDivisor {
+    /**
+     * @Override - переопределение метода
+     * Метод возвращает наибольший делитель 2-ух чисел. (Алгоритм Евклида)
+     * @param firstNumber - Первое число
+     * @param secondNumber - Второе число
+     * @return - наибольший делитель
+     */
     @Override
     public int getDivisor(int firstNumber, int secondNumber) {
         while (firstNumber != 0) {
--- module01/src/main/java/ru/sberbank/edu/impl/GreetingImpl.java
+++ module01/src/main/java/ru/sberbank/edu/impl/GreetingImpl.java
@@ -1,8 +1,15 @@
 package ru.sberbank.edu.impl;
 
 import ru.sberbank.edu.Greeting;
-
+/**
+ * Класс GreetingImpl реализует интерфейс Greeting
+ * @author Максим Радюшин{
+ */
 public class GreetingImpl implements Greeting {
+    /**
+     * @Override - переопределение метода
+     * Метод возвращает информация обо мне
+     */
     @Override
     public String getBestHobby() {
         return "Привет! Меня зовут Радюшин Максим и я работаю в Сбербанке. Мое главное хобби - Футбол";
